aa058a619beec3f5f432d25299e6ca35898e4f90
Antonin <antonin.hausseguy@gmail.com>
2023-06-30 16:58:05 +0200

    correction merge

--- Index.py
+++ Index.py
@@ -5,4 +5,4 @@ date = "22/09/1998"
 # OPTIONS WORDS : lower | upper | first_letter |remove_accent 
 # OPTIONS DATE : combineall | month_to_string | two_digits_year | four_digits_year
 
-Merge('slt','22/09/1998',[['combineall', True], ['month_to_string' , True]]).merge()
+Merge('slt','22/09/1998',combineall = True, ).mergeDate()
